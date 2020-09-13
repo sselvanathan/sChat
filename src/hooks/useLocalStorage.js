250323df6f23b1f734fff09c26bfc5045913b48d
Senthu <senthuu27@gmail.de>
2020-09-13 12:34:35 +0200

    init

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,21 @@
+import {useEffect, useState} from 'react'
+
+const PREFIX = 'sChat'
+export default function useLocalStorage(key, initialvalue) {
+    const prefixedKey = PREFIX + key
+    const [value, setValue] = useState(() => {
+        const jsonValue = localStorage.getItem(prefixedKey)
+
+        if (jsonValue != null) return JSON.parse(jsonValue)
+        if (typeof initialvalue === 'function') {
+            return initialvalue()
+        } else {
+            return initialvalue
+        }
+    })
+
+    useEffect(() => {
+    localStorage.setItem(prefixedKey, JSON.stringify(value))
+    }, [prefixedKey, value])
+    return [value, setValue]
+}
